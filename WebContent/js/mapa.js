8e10ac5a71ce52a72f05e105f045dea7c0c920db
vsnepomuceno <vnepomuceno@gmail.com>
2014-05-05 17:59:45 -0300

    [NA]:

--- WebContent/js/mapa.js
+++ WebContent/js/mapa.js
@@ -13,6 +13,20 @@ function carregarJSON() {
 		            url: 'http://soservices.vsnepomuceno.cloudbees.net/prestador',
 		            contentType: "application/json; charset=utf-8",
 		            dataType: "json",
+		            xhrFields: {
+		                // The 'xhrFields' property sets additional fields on the XMLHttpRequest.
+		                // This can be used to set the 'withCredentials' property.
+		                // Set the value to 'true' if you'd like to pass cookies to the server.
+		                // If this is enabled, your server must respond with the header
+		                // 'Access-Control-Allow-Credentials: true'.
+		                withCredentials: false
+		              },
+
+		              headers: {
+		                // Set any custom headers here.
+		                // If you set any non-simple headers, your server must include these
+		                // headers in the 'Access-Control-Allow-Headers' response header.
+		              },
 		            success: function (data) {
 		            	$.each(data.list, function (i, theItem) {
 		            		if (i != "@id") {

b28f4c74c207a2cb37decf501fa1641594439fdc
vsnepomuceno <vnepomuceno@gmail.com>
2014-05-09 17:12:02 -0300

    [NA]

--- WebContent/js/mapa.js
+++ WebContent/js/mapa.js
@@ -30,7 +30,7 @@ function carregarJSON() {
 	 $.ajax(
 		        {		        	 
 		            type: "GET",
-		            url: 'http://localhost:8080/sos-api/prestador',
+		            url: 'http://soservices.vsnepomuceno.cloudbees.net/prestador',
 		            contentType: "application/json; charset=utf-8",
 		            dataType: "jsonp",
 		            crossDomain: true,		         	            
